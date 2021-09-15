1f25f70ee577d18d64af6ed96465dc78acfa6a10
Charles <86209096+Prchazz@users.noreply.github.com>
2021-09-15 12:21:49 -0400

    update json

---
+++ CodingEventsAPI/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+
+namespace CodingEventsAPI {
+  public class Program {
+    public static void Main(string[] args) {
+      CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) {
+      return Host.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration(
+          (context, config) => {
+            // only use keyvault in production, user-secrets for local dev
+            if (!context.HostingEnvironment.IsProduction()) return;
+            
+            var builtConfig = config.Build();
+
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var keyVaultClient = new KeyVaultClient(
+              new KeyVaultClient.AuthenticationCallback(
+                azureServiceTokenProvider.KeyVaultTokenCallback
+              )
+            );
+
+            config.AddAzureKeyVault(
+              $"https://{builtConfig["KeyVaultName"]}.vault.azure.net/",
+              keyVaultClient,
+              new DefaultKeyVaultSecretManager()
+            );
+          }
+        )
+        .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+  }
+}
